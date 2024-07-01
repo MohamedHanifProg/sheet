3284227be1e6db80c99869e4e9caf72053afa7c1
MohamedHanifProg <signutre.31@gmail.com>
2024-07-02 02:47:56 +0300

    first commit

--- server/routes.js
+++ server/routes.js
@@ -167,12 +167,25 @@ router.delete('/items/:id', (req, res) => {
 
 // Serve the profileGraph.json file
 router.get('/profile-graph-data', (req, res) => {
-  res.sendFile(path.join(__dirname, 'data', 'profileGraph.json'));
+  const filePath = path.join(__dirname, '..', 'data', 'profileGraph.json');
+  res.sendFile(filePath, (err) => {
+    if (err) {
+      logger.error(`Error serving profileGraph.json: ${err.message}`);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
 });
 
 // Serve the homeGraph.json file
 router.get('/home-graph-data', (req, res) => {
-  res.sendFile(path.join(__dirname, 'data', 'homeGraph.json'));
+  const filePath = path.join(__dirname, '..', 'data', 'homeGraph.json');
+  res.sendFile(filePath, (err) => {
+    if (err) {
+      logger.error(`Error serving homeGraph.json: ${err.message}`);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
 });
+
 
 module.exports = router;
