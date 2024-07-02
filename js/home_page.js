7d19ef27b3c7b515be87f72c7a7384103de0aabc
MohamedHanifProg <signutre.31@gmail.com>
2024-07-02 15:26:08 +0300

    asd as

--- js/home_page.js
+++ js/home_page.js
@@ -1,4 +1,6 @@
-const API_URL = window.location.hostname === 'localhost' ? 'http://localhost:3000/api' : `https://${window.location.hostname}/api`;
+const API_URL = window.location.protocol === 'http:' 
+    ? `http://${window.location.hostname}/api` 
+    : `https://${window.location.hostname}/api`;
 
 window.onload = () => {
     populateItemsTable();
