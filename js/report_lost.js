b12826f10d842fa54ad6cb72fc603436faf40cf2
MohamedHanifProg <signutre.31@gmail.com>
2024-07-02 17:12:11 +0300

    asd as

--- js/report_lost.js
+++ js/report_lost.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
   form.addEventListener('submit', handleFormSubmit);
 });
 
-const API_URL = window.location.hostname === 'localhost' ? 'http://localhost:3000/api' : 'https://sheet-fqwb.onrender.com/api';
+const API_URL = window.location.hostname === 'localhost' ? 'http://localhost:3000/api' : 'https://lost-and-found-project.onrender.com';
 
 function getItemDataFromForm(formData, imageUrl) {
   const userId = 1; 
